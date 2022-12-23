64e8d8dd1b005392c69750215f706fe41703773b
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2022-12-23 09:38:21 -0600

    fix

--- setup.py
+++ setup.py
@@ -13,17 +13,16 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
  
 setup(
-    name="Beardb",
-    version="0.0.3",
-    description="A Local System Based JSON Database system",
+    name="beardb",
+    version="0.0.1",
+    description="Database system implementing encrypted versions JSON of data. Easy to access, manage and deploy remotely",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/teddyoweh/Beardb",
     author="Teddy Oweh",
     author_email="teddy@teddyoweh.net",
-    packages=find_packages('src'),
-    package_dir={'': 'src/Beardb'},
-    license="MIT",
+    packages=find_packages('src/beardb/'),
+    package_dir={'': 'src/beardb/'},
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
@@ -31,7 +30,7 @@ setup(
     ],
   
     include_package_data=True,
-    install_requires=['cryptography','dotenv','requests'],
+    install_requires=['cryptography','requests'],
     entry_points={
         "console_scripts": [
             "Beardb=reader.__main__:main",

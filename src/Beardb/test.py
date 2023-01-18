84bcbbdc16660bd78abca2c64e8ed0de6ffd0ba7
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2023-01-18 14:16:37 -0600

    fix bugs

--- src/Beardb/test.py
+++ src/Beardb/test.py
@@ -1,30 +1,11 @@
-from Beardb import Beardb
-from Bucket import Bucket
-from Client import Client
+from Beardb import *
+from Bucket import *
 
-data = Beardb('sodd')
-data.load_database('windowsapp')
-newjson = Bucket(project=data,bucket_name='people')
-# newjson.schemas(
-#     {
-#        'type':str,
-#        'id':int,
-#        'name':str,
-#        'email':str,
-#        'list':list
 
-#     }
-# )
-newjson.insert(data={"name":"oweh","amountpaid":400,"email":"tedz@gmail.com"})
-# newjson.update(query={'email': 'tedz@gmail.com'},data={'name':'amaka done blow'})
-# newjson.updatebyId(id='48b27e8c-7db5-11ed-9e76-324d38bf6d76',data={"name":"james","amountpaid":1555500})
-print(newjson.fetchData())
-# import json
-# name = '{"name":"lucas","amountpaid":500,"email}'
+project = Beardb('random_project')
+project.load_database('class') 
 
-# names = json.loads
-# print(names)\\
-# from cryptography.fernet import Fernet
-# print(Fernet.generate_key())
-
-# open('key.key','wb').write(Fernet.generate_key())
+randomshit = Bucket(project,'randomshit')
+print(randomshit.fetchData({}))
+randomshit.updatebyId('b4e57d98-93c2-11ed-b0d6-324d38bf6d76',{'age:': 20})
+print(randomshit.fetchData({}))
